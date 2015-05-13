7cc2958f6d1002db55367693726b0cf00e83d1d7
James Reidy <jreidy@cornell.edu>
2015-05-13 15:59:58 -0400

    add photo_years field to village - lists years with photos from that village

--- plugins/restful/RestfulVillageMetadataResource.class.php
+++ plugins/restful/RestfulVillageMetadataResource.class.php
@@ -45,6 +45,51 @@ class RestfulVillageMetadataResource extends RestfulEntityBaseNode {
       'sub_property' => 'geom',
     );
 
+    $public_fields['photo_years'] = array(
+      'callback' => 'static::photo_years',
+    );
+
     return $public_fields;
   }
+
+  public static function photo_years($wrapper) {
+    // find the nids of image_metadata nodes for this village
+    $nid = $wrapper->nid->value();
+    $efq = new EntityFieldQuery();
+    $result = $efq
+      ->entityCondition('entity_type', 'node')
+      ->entityCondition('bundle', 'image_metadata')
+      ->fieldCondition('field_village_reference', 'target_id', $nid, '=')
+      ->fieldOrderBy('field_photo_year', 'value', 'ASC')
+      ->addMetaData('account', user_load(1))
+      ->execute();
+
+    if (isset($result['node'])) {
+      $images = $result['node'];
+
+      // At first we need to get field's id. If you already know field id, you can ommit this step
+      // Get all fields attached to a given node type
+      $fields = field_info_instances('node', 'image_metadata');
+
+      // Get id of body field
+      $photo_year_field_id = $fields['field_photo_year']['field_id'];
+
+      // Attach a field of selected id only to get value for it
+      field_attach_load('node', $images, FIELD_LOAD_CURRENT, array('field_id' => $photo_year_field_id));
+
+      $known_year = array();
+      foreach ($images as $nid => $node) {
+        $year = $node->field_photo_year[LANGUAGE_NONE][0]['value'];
+        $known_year["$year"] = 1;
+      }
+      // get values of our field
+      $years = array_keys($known_year);
+    }
+    else {
+      $years = array();
+    }
+
+   return $years;
+  }
+
 }
